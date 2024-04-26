c4bbbd59f626baf6c91bcc244793616ceb026c54
Hugo-Leite <ribeirohugo946@gmail.com>
2024-04-25 22:50:45 -0300

    javascript finalizado

--- script.js
+++ script.js
@@ -3,32 +3,71 @@ let tipAmountPerPerson = 0;
 let totalAmountWithTip = 0;
 let totalPricePerPerson = 0;
 
+let tipFee = document.querySelector("#tip-fee");
 const btnCalculate = document.querySelector(".calculate");
 let selectedPercentage = 0;
+
+function formatedNumber(number) {
+    return number.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
+}
 
 function selectPercentage(percentage) {
     selectedPercentage = percentage;
 
-    document.querySelectorAll(".btn-rate").forEach(function removeClassActive(button) {
+    document.querySelectorAll(".btn-rate").forEach((button) => {
         button.classList.remove("active");
     });
 
     event.target.classList.add("active");
 }
 
+tipFee.addEventListener("focus", () => {
+    document.querySelectorAll(".btn-rate").forEach((button) => {
+        button.classList.remove("active");
+    });
+});
+
+tipFee.addEventListener("blur", () => {
+    selectedPercentage = tipFee.value;
+});
+
 btnCalculate.addEventListener("click", () => {
     const value = document.querySelector("#total");
     const numberPeople = document.querySelector("#number-people");
 
-    totalTipAmount = (value.value * selectedPercentage) / 100;
-    console.log(`Valor da gorjeta: ${totalTipAmount}`);
+    if (value.value === "" || numberPeople.value === "") {
+        alert("Voçe precisa preencher os dados");
+    } else {
+        totalTipAmount = (value.value * parseFloat(selectedPercentage)) / 100;
+        document.querySelector(".tip-total").textContent = formatedNumber(totalTipAmount);
 
-    tipAmountPerPerson = selectedPercentage / numberPeople.value;
-    console.log(`Valor da gorjeta por pessoa: ${tipAmountPerPerson}`);
+        tipAmountPerPerson = parseFloat(selectedPercentage) / numberPeople.value;
+        document.querySelector(".tip-per-person").textContent = formatedNumber(tipAmountPerPerson);
 
-    totalAmountWithTip = parseFloat(value.value) + selectedPercentage;
-    console.log(`Valor total com gorjeta: ${totalAmountWithTip}`);
+        totalAmountWithTip = parseFloat(value.value) + parseFloat(selectedPercentage);
+        document.querySelector(".total-with-tip").textContent = formatedNumber(totalAmountWithTip);
 
-    totalPricePerPerson = totalAmountWithTip / numberPeople.value;
-    console.log(`Valor total por pessoa: ${totalPricePerPerson}`);
+        totalPricePerPerson = totalAmountWithTip / numberPeople.value;
+        document.querySelector(".total-per-person").textContent =
+            formatedNumber(totalPricePerPerson);
+    }
+
+    value.value = "";
+    numberPeople.value = "";
+    tipFee.value = "";
+    document.querySelectorAll(".btn-rate").forEach((button) => {
+        button.classList.remove("active");
+    });
+});
+
+document.querySelector("#reset").addEventListener("click", () => {
+    const reset = 0;
+
+    document.querySelector(".tip-total").textContent = formatedNumber(reset);
+
+    document.querySelector(".tip-per-person").textContent = formatedNumber(reset);
+
+    document.querySelector(".total-with-tip").textContent = formatedNumber(reset);
+
+    document.querySelector(".total-per-person").textContent = formatedNumber(reset);
 });
